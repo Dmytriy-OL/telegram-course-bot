d83638961fe24befdf766bf8852d966bcaf2bdd7
Dima Oliinyk <vt221_ods@student.ztu.edu.ua>
2025-05-26 20:47:24 +0300

    Added a keyboard for the main admin and changed the permissions for the teachers.

--- app/keyboards/keyboards.py
+++ app/keyboards/keyboards.py
@@ -8,6 +8,7 @@ def back_button_builder(text: str = '🔙 Назад') -> InlineKeyboardBuilder:
     keyboard.add(InlineKeyboardButton(text=text, callback_data="delete_previous_message"))
     return keyboard
 
+
 def back_button_markup():
     """Функція для створення клавіатури з кнопкою 'Назад'."""
     return InlineKeyboardMarkup(
@@ -25,10 +26,22 @@ def get_inline_keyboard():
     return keyboard
 
 
-def get_admin_command():
+def get_teachers_command():
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="👨‍🏫 Для викладачів", callback_data="teachers")],
+            [InlineKeyboardButton(text="👨🏻‍💻 Панель користувача", callback_data="go_to_main_menu")],
+            [InlineKeyboardButton(text="🔙 Назад", callback_data="pass")],
+        ]
+    )
+    return keyboard
+
+
+def main_admin_command():
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
             [InlineKeyboardButton(text="🛡️ Адміни", callback_data="admin_admins")],
+            [InlineKeyboardButton(text="👨🏻‍💻‍ Викладачі", callback_data="add_teacher")],
             [InlineKeyboardButton(text="📊 Статистика", callback_data="admin_stats")],
             [InlineKeyboardButton(text="📝 Зміни головного текст", callback_data="edit_main_text")],
             [InlineKeyboardButton(text="🖼️ Зміни зображення", callback_data="edit_main_image")],
