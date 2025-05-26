a4377ccf679c808565089dd35844f38460a448f1
Dima Oliinyk <vt221_ods@student.ztu.edu.ua>
2025-05-27 00:06:27 +0300

    added a router for teachers

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -4,6 +4,7 @@ from aiogram import Router
 from .commands import router as commands_router
 from .admin import router as admin_router
 from .callbacks import router as callbacks_router
+from .teachers import router as teachers_router
 # Створюємо загальний маршрутизатор
 router = Router()
 
@@ -12,3 +13,4 @@ router = Router()
 router.include_router(commands_router)
 router.include_router(callbacks_router)
 router.include_router(admin_router)
+router.include_router(teachers_router)
