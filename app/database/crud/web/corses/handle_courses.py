e9f1df4c7f549ba18e9840f0a64a2a91bc6c8132
Dima Oliinyk <doliynyk1@gmail.com>
2025-10-15 19:50:30 +0300

    feat(courses): add support for course avatars

--- app/database/crud/web/corses/handle_courses.py
+++ app/database/crud/web/corses/handle_courses.py
@@ -2,16 +2,18 @@ from sqlalchemy import update, delete, and_
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
 
-from app.database.core.models import Courses, Administrator, Module, Task, Answer
+from app.database.core.models import Courses, Administrator, Module, Task, Answer, CourseAvatar
 from app.database.core.models import SessionLocal
 
 from datetime import date
 
 
-async def create_course(title: str, price: int, caption: str, lesson_count: int, teacher_id: int):
+async def create_course(title: str, save_image: str, price: int, caption: str, lesson_count: int, teacher_id: int):
     async with SessionLocal() as session:
+        course_avatar = CourseAvatar(file_name=save_image)
+
         add_course = Courses(title=title, price=price, caption=caption, lesson_count=lesson_count,
-                             teacher_id=teacher_id)
+                             teacher_id=teacher_id, avatar=course_avatar)
         session.add(add_course)
         await session.commit()
         return True
