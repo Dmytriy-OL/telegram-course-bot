596fda87dd928dc140a2cdcbbeef50760a2377af
Dima Oliinyk <doliynyk1@gmail.com>
2025-10-15 19:52:02 +0300

    feat(courses): add dedicated function for saving course avatars

--- app/web/utils/image_handler.py
+++ app/web/utils/image_handler.py
@@ -1,4 +1,4 @@
-from app.database.core.paths import AVATAR_DIR
+from app.database.core.paths import AVATAR_DIR, AVATAR_COURSES_DIR
 from fastapi import UploadFile
 import uuid
 import shutil
@@ -11,5 +11,27 @@ async def save_image_to_disk_and_db(avatar: UploadFile):
 
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(avatar.file, buffer)
+
+    return file_name
+
+
+async def save_image_to_course_folder(course_avatar: UploadFile, title: str):
+    _, ext = course_avatar.filename.rsplit(".", 1)
+    ext = ext.lower()
+
+    allowed_ext = {"jpg", "jpeg", "png", "webp"}
+    if ext not in allowed_ext:
+        raise ValueError(f"Unsupported image format: {ext}")
+
+    file_name = f"{title}.{ext}"
+
+    course_folder = AVATAR_COURSES_DIR / title
+    course_folder.mkdir(parents=True, exist_ok=True)
+
+    file_path = course_folder / file_name
+
+    with file_path.open("wb") as buffer:
+        content = await course_avatar.read()
+        buffer.write(content)
 
     return file_name
