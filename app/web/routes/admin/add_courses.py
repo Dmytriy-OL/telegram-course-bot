c1f5e4a86813c431247532365fffe0b9c961cb8a
Dima Oliinyk <doliynyk1@gmail.com>
2025-10-15 19:46:47 +0300

    feat(courses): add image upload step before preview integration

--- app/web/routes/admin/add_courses.py
+++ app/web/routes/admin/add_courses.py
@@ -1,5 +1,5 @@
 import logging
-from fastapi import APIRouter, Request, Form, Depends
+from fastapi import APIRouter, Request, Form, Depends, UploadFile, File
 from fastapi.responses import RedirectResponse, HTMLResponse
 
 from app.database.crud.web.administrator.handle_administrator import all_administrators
@@ -8,6 +8,7 @@ from app.web.dependencies.auth_dependencies import validate_login_form
 from app.database.core.models import User
 from app.web.schemas.forms import CoursesForm
 from app.web.templates import templates
+from app.web.utils.image_handler import save_image_to_course_folder
 
 router = APIRouter()
 
@@ -15,12 +16,13 @@ router = APIRouter()
 @router.get("/courses_add", response_class=HTMLResponse)
 async def courses_add_get(request: Request):
     administrators = await all_administrators()
-    return templates.TemplateResponse("courses_add.html", {"request": request,"teachers": administrators})
+    return templates.TemplateResponse("courses_add.html", {"request": request, "teachers": administrators})
 
 
 @router.post("/courses_add", response_class=HTMLResponse)
 async def courses_add_post(request: Request,
                            title: str = Form(...),
+                           course_avatar: UploadFile = File(None),
                            price: int = Form(...),
                            caption: str = Form(...),
                            lesson_count: int = Form(...),
@@ -28,13 +30,29 @@ async def courses_add_post(request: Request,
                            ):
     try:
         form_data = CoursesForm(price=price)
-        await create_course(title, form_data.price, caption, lesson_count, teacher_id)
+
+        if course_avatar is None:
+            save_image = None
+        else:
+            save_image = await save_image_to_course_folder(course_avatar, title)
+
+        await create_course(title, save_image, form_data.price, caption, lesson_count, teacher_id)
+
         return templates.TemplateResponse("courses_add.html", {
             "request": request,
             "success": "Курс успішно додано!",
         })
+
     except ValueError as e:
         return templates.TemplateResponse("courses_add.html", {
             "request": request,
             "error": str(e)
+        })
+
+    except Exception as e:
+        # Логування і показ більш загальної помилки
+        print(f"Error adding course: {e}")
+        return templates.TemplateResponse("courses_add.html", {
+            "request": request,
+            "error": "Сталася помилка при додаванні курсу."
         })
