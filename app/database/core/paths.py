7da1598cd46d0f61112f390ccc59cebb3877db84
Dima Oliinyk <doliynyk1@gmail.com>
2025-10-15 19:55:28 +0300

    Add folder for saving avatar course

--- app/database/core/paths.py
+++ app/database/core/paths.py
@@ -7,6 +7,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 STATIC_DIR = BASE_DIR / "web" / "static"
 STATIC_DIR.mkdir(parents=True, exist_ok=True)
 
-# Папка для зберігання аватарок
+# Папка для зберігання аватарок користувача
 AVATAR_DIR = STATIC_DIR / "images" / "avatars"
 AVATAR_DIR.mkdir(parents=True, exist_ok=True)
+
+
+# Папка для зберігання аватарок курсу
+AVATAR_COURSES_DIR = STATIC_DIR / "images" / "courses_avatars"
+AVATAR_COURSES_DIR.mkdir(parents=True, exist_ok=True)
