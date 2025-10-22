4552b62f08edc71766b775d4d754c7104df752cf
Dima Oliinyk <doliynyk1@gmail.com>
2025-10-22 14:46:21 +0300

    feat: added video saving to module folder with error handling

--- app/web/utils/image_handler.py
+++ app/web/utils/image_handler.py
@@ -1,7 +1,12 @@
-from app.database.core.paths import AVATAR_DIR, AVATAR_COURSES_DIR
+import logging
+
+from app.database.core.paths import AVATAR_DIR, CREATION_COURSES_DIR
 from fastapi import UploadFile
 import uuid
 import shutil
+from fastapi import HTTPException
+
+from app.database.crud.web.corses.handle_courses import get_course_by_id, create_video_record
 
 
 async def save_image_to_disk_and_db(avatar: UploadFile):
@@ -25,7 +30,7 @@ async def save_image_to_course_folder(course_avatar: UploadFile, title: str):
 
     file_name = f"{title}.{ext}"
 
-    course_folder = AVATAR_COURSES_DIR / title
+    course_folder = CREATION_COURSES_DIR / title / "courses_avatars"
     course_folder.mkdir(parents=True, exist_ok=True)
 
     file_path = course_folder / file_name
@@ -35,3 +40,28 @@ async def save_image_to_course_folder(course_avatar: UploadFile, title: str):
         buffer.write(content)
 
     return file_name
+
+
+async def save_video_to_module_folder(video: UploadFile, description: str, course_id: int, module_id: int):
+    try:
+        course_title = await get_course_by_id(course_id)
+    except ValueError as e:
+        logging.error(f"Помилка при отриманні курсу: {e}")
+        raise HTTPException(status_code=400, detail=str(e))
+
+    video_path = CREATION_COURSES_DIR / course_title / f"module_{module_id}" / "video"
+    video_path.mkdir(parents=True, exist_ok=True)
+
+    file_path = video_path / video.filename
+
+    with file_path.open("wb") as buffer:
+        content = await video.read()
+        buffer.write(content)
+
+    await create_video_record(
+        description=description,
+        module_id=module_id,
+        video_file=video.filename
+    )
+
+    return True
