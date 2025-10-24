51c359527b12f0ed038dae8e85e5f3d96e5c9e63
Dima Oliinyk <doliynyk1@gmail.com>
2025-10-24 18:24:04 +0300

    feat: add select_course router to main APIRouter

--- app/web/routes/__init__.py
+++ app/web/routes/__init__.py
@@ -6,6 +6,8 @@ from app.web.routes.forgot_password import router as router_forgot_password
 from app.web.routes.admin.add_courses import router as admin_add_courses
 from app.web.routes.admin.create_administrator import router as admin_add_administrator
 from app.web.routes.admin.create_module_courses import router as create_module_courses
+from app.web.routes.admin.select_course import router as select_course
+
 
 router = APIRouter()
 
@@ -17,3 +19,5 @@ router.include_router(router_forgot_password)
 router.include_router(admin_add_courses)
 router.include_router(admin_add_administrator)
 router.include_router(create_module_courses)
+router.include_router(select_course)
+
