203998153a77b724a600a3395d9c53845b21a2d0
Dima Oliinyk <doliynyk1@gmail.com>
2025-11-04 22:21:10 +0200

    chore: clear imports

--- app/web/routes/profile.py
+++ app/web/routes/profile.py
@@ -1,19 +1,12 @@
 import os
 from fastapi import APIRouter, Request, Form, Depends, UploadFile, File
-from fastapi.responses import RedirectResponse, HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from datetime import datetime
-from app.database.core.models import User, SessionLocal
+from app.database.core.models import User
 from app.database.crud.web.repository.user_repo import is_username_taken, update_user_data, update_user_avatar, \
-    get_user_by_email, fetch_updated_user_with_avatar
-from app.web.dependencies.auth_dependencies import get_current_user, get_authenticated_user
+ fetch_updated_user_with_avatar
+from app.web.dependencies.auth_dependencies import get_authenticated_user
 from app.web.templates import templates
-from app.database.core.paths import STATIC_DIR
-from pathlib import Path
-import uuid
-import shutil
-from sqlalchemy.future import select
-from sqlalchemy.orm import selectinload
 
 from app.web.utils.image_handler import save_image_to_disk_and_db
 
