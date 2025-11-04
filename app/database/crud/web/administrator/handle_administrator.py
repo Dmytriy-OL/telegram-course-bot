839ead9477f1548d123a6e8284ea123f689b0d75
Dima Oliinyk <doliynyk1@gmail.com>
2025-11-04 22:24:52 +0200

    refactor(admin): clean imports and add return type hint to create_administrator

--- app/database/crud/web/administrator/handle_administrator.py
+++ app/database/crud/web/administrator/handle_administrator.py
@@ -1,14 +1,10 @@
-from sqlalchemy import update, delete, and_
 from sqlalchemy.future import select
-from sqlalchemy.orm import selectinload
 
 from app.database.core.models import Administrator
 from app.database.core.models import SessionLocal
 
-from datetime import date
 
-
-async def create_administrator(tg_id: int, name: str, surname: str, login: str, main_admin: bool):
+async def create_administrator(tg_id: int, name: str, surname: str, login: str, main_admin: bool) -> bool:
     async with SessionLocal() as session:
         add_course = Administrator(tg_id=tg_id, name=name, surname=surname, login=login,
                                    main_admin=main_admin)
