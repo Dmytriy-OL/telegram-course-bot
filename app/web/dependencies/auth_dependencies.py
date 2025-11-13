86ece4ba458eea80eecd9954ccf35f4bb695b37e
Dima Oliinyk <doliynyk1@gmail.com>
2025-11-14 00:20:36 +0200

    feat(auth): improved user experience by removing unnecessary form fields

--- app/web/dependencies/auth_dependencies.py
+++ app/web/dependencies/auth_dependencies.py
@@ -1,7 +1,4 @@
-import os
-
 from fastapi import Request, HTTPException, status, Form
-from authlib.integrations.starlette_client import OAuth
 from starlette.responses import HTMLResponse
 
 from app.database.crud.web.repository.user_repo import get_user_by_email, user_exists
@@ -10,8 +7,6 @@ from app.database.core.models import User
 from app.web.templates import templates
 from app.web.schemas.forms import RegisterForm
 from pydantic import ValidationError
-
-oauth = OAuth()
 
 
 async def get_current_user(request: Request) -> User:
@@ -29,15 +24,15 @@ async def get_current_user(request: Request) -> User:
 async def validate_login_form(request: Request, email: str = Form(...),
                               password: str = Form(...)) -> User | HTMLResponse:
     auth_status, user = await authenticate_user(email, password)
-    print("AUTH:", auth_status, user)
     match auth_status:
         case "not_found":
-            return templates.TemplateResponse("login.html", {"request": request, "error": "Користувача не знайдено",
-                                                             "email": email},
+            return templates.TemplateResponse("auth/login.html",
+                                              {"request": request, "error": "Користувача не знайдено",
+                                               "email": email},
                                               status_code=400)
         case "wrong_password":
-            return templates.TemplateResponse("login.html", {"request": request, "error": "Невірний пароль",
-                                                             "email": email},
+            return templates.TemplateResponse("auth/login.html", {"request": request, "error": "Невірний пароль",
+                                                                  "email": email},
                                               status_code=400)
         case "ok":
             return user
@@ -58,22 +53,22 @@ async def get_authenticated_user(request: Request) -> User | None:
 
 
 async def parse_register_form(
-        birth_day: int = Form(...),
-        birth_month: int = Form(...),
-        birth_year: int = Form(...),
+        # birth_day: int = Form(...),
+        # birth_month: int = Form(...),
+        # birth_year: int = Form(...),
         email: str = Form(...),
-        username: str = Form(..., alias="login"),
+        # username: str = Form(..., alias="login"),
         password: str = Form(...),
         password_confirm: str = Form(...),
         terms: bool = Form(...)
 ):
     try:
         form_data = RegisterForm(
-            birth_day=birth_day,
-            birth_month=birth_month,
-            birth_year=birth_year,
+            # birth_day=birth_day,
+            # birth_month=birth_month,
+            # birth_year=birth_year,
             email=email,
-            username=username,
+            # username=username,
             password=password,
             password_confirm=password_confirm,
             terms=terms,
@@ -81,14 +76,7 @@ async def parse_register_form(
     except ValidationError as e:
         raw_msg = e.errors()[0].get('msg', 'Помилка валідації')
         error_message = raw_msg.replace("Value error, ", "")
-        raise ValueError(error_message)
+        return {"error": error_message}
     return form_data
 
 
-oauth.register(
-    name="google",
-    client_id=os.getenv("GOOGLE_CLIENT_ID"),
-    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
-    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
-    client_kwargs={"scope": "openid email profile"},
-)
