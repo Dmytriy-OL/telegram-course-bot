89b707b65680f203b63b93e8bebe5758501a2367
Dima Oliinyk <doliynyk1@gmail.com>
2025-11-26 12:03:11 +0200

    feat(router): add profile_settings and google oauth routes

--- app/web/routes/routes.py
+++ app/web/routes/routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from app.web.routes.auth.register import router as router_register
 from app.web.routes.profile.profile import router as router_profile
 from app.web.routes.profile.profile_setup import router as router_profile_setup
+from app.web.routes.profile.profile_settings import router as profile_settings
 from app.web.routes.auth.login import router as router_login
 from app.web.routes.forgot_password import router as router_forgot_password
 from app.web.routes.courses import router as router_courses
@@ -14,12 +15,17 @@ from app.web.routes.teacher.become_teacher import router as become_teacher
 from app.web.routes.pages.info import router as info
 from app.web.routes.pages.legal import router as legal
 
+from app.web.routes.auth.google_oauth import router as google_oauth
+
 router = APIRouter()
 
 router.include_router(router_register)
-router.include_router(router_profile)
-router.include_router(router_profile_setup)
 router.include_router(router_login)
+router.include_router(google_oauth)
+
+router.include_router(router_profile)
+router.include_router(profile_settings)
+router.include_router(router_profile_setup)
 router.include_router(router_forgot_password)
 router.include_router(router_courses)
 
