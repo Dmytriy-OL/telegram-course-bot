af65dd53d018148ada5dbf0906a010aaa26e8ee7
Dima Oliinyk <doliynyk1@gmail.com>
2026-01-01 23:28:05 +0200

    feat(templates): add user context to info pages

--- app/web/routes/pages/info.py
+++ app/web/routes/pages/info.py
@@ -1,21 +1,23 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
+
+from app.web.dependencies.template_dependencies import get_template_user
 from app.web.templates import templates
 
 router = APIRouter()
 
 
 @router.get("/learn-more", response_class=HTMLResponse)
-async def learn_more(request: Request):
+async def learn_more(request: Request, user=Depends(get_template_user)):
     return templates.TemplateResponse(
         "pages/learn_more.html",
-        {"request": request}
+        {"request": request, "user": user}
     )
 
 
 @router.get("/why_us", response_class=HTMLResponse)
-async def learn_more(request: Request):
+async def learn_more(request: Request, user=Depends(get_template_user)):
     return templates.TemplateResponse(
         "pages/why_us.html",
-        {"request": request}
+        {"request": request, "user": user}
     )
