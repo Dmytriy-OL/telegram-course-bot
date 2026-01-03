81f648d106ef77d7df67974fd651ead9359bd599
Dima Oliinyk <doliynyk1@gmail.com>
2026-01-03 18:49:38 +0200

    refactor: Use get_session_user dependency for static pages

--- app/web/routes/pages/info.py
+++ app/web/routes/pages/info.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 
-from app.web.dependencies.template_dependencies import get_template_user
+from app.web.dependencies.template_dependencies import get_session_user
 from app.web.templates import templates
 
 router = APIRouter()
 
 
 @router.get("/learn-more", response_class=HTMLResponse)
-async def learn_more(request: Request, user=Depends(get_template_user)):
+async def learn_more(request: Request, user=Depends(get_session_user)):
     return templates.TemplateResponse(
         "pages/learn_more.html",
         {"request": request, "user": user}
@@ -16,7 +16,7 @@ async def learn_more(request: Request, user=Depends(get_template_user)):
 
 
 @router.get("/why_us", response_class=HTMLResponse)
-async def learn_more(request: Request, user=Depends(get_template_user)):
+async def learn_more(request: Request, user=Depends(get_session_user)):
     return templates.TemplateResponse(
         "pages/why_us.html",
         {"request": request, "user": user}
