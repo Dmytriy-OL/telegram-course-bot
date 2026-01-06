a34e435a4030e30aa6275a0230345493e3d31fcf
Dima Oliinyk <doliynyk1@gmail.com>
2026-01-06 21:19:05 +0200

    feat(auth): add get_current_teacher helper to fetch logged-in teacher with related data

--- app/web/dependencies/template_dependencies.py
+++ app/web/dependencies/template_dependencies.py
@@ -1,8 +1,10 @@
 from fastapi import Request, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.database.core.models import User
+from app.database.core.models import User, Teacher
 from app.web.dependencies.db import get_db
+from sqlalchemy.future import select
+from sqlalchemy.orm import joinedload, selectinload
 
 
 def get_session_user(request: Request):
@@ -10,11 +12,23 @@ def get_session_user(request: Request):
 
 
 async def get_current_user(
-    request: Request,
-    db: AsyncSession = Depends(get_db)
+        request: Request,
+        db: AsyncSession = Depends(get_db)
 ):
     user_data = request.session.get("user")
     if not user_data:
         return None
 
     return await db.get(User, user_data["id"])
+
+
+async def get_current_teacher(request: Request, db: AsyncSession = Depends(get_db)):
+    user_data = request.session.get("user")
+    if not user_data:
+        return None
+    result = await db.execute(
+        select(User)
+        .options(selectinload(User.avatar),selectinload(User.teacher).selectinload(Teacher.languages))
+        .where(User.id == user_data["id"])
+    )
+    return result.scalars().first()
