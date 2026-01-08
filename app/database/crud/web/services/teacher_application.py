942f55f00964941ce72895de510180d5c0693dc5
Dima Oliinyk <doliynyk1@gmail.com>
2026-01-08 21:37:05 +0200

    feat:Added TeacherApplicationService and ProfileService

--- app/database/crud/web/services/teacher_application.py
+++ app/database/crud/web/services/teacher_application.py
@@ -1,6 +1,11 @@
-from app.web.schemas.teacher import TeacherApplicationCreate
-from app.database.core.models import User
+from fastapi import UploadFile
+
+from app.database.crud.web.teacher.exceptions import UserAlreadyTeacherError
+from app.database.crud.web.user.repository import UserRepository
+from app.web.schemas.teacher import TeacherApplicationCreate, ProfileUpdateData
+from app.database.core.models import User, UserAvatar
 from app.database.crud.web.teacher.repository import TeacherRepository
+from app.web.utils.image_handler import save_avatar_to_disk
 
 
 class TeacherApplicationService:
@@ -9,14 +14,38 @@ class TeacherApplicationService:
         self.repo = repo
 
     async def apply(self, user: User, data: TeacherApplicationCreate):
-        # тут може бути:
-        # - валідація
+        # 🔐 бізнес-перевірки
+        if user.is_teacher:
+            raise UserAlreadyTeacherError("Ви вже маєте статус викладача")
+
+        if not data.languages:
+            raise ValueError("Потрібно додати хоча б одну мову")
+
+        # 🧠 тут можуть бути:
+        # - moderation
         # - логування
         # - нотифікації
-        # - moderation workflow
-        await self.repo.create_teacher(user.id, data)
+
+        return await self.repo.create_teacher(user, data)
 
 
-teacher_application_service = TeacherApplicationService(
-    repo=TeacherRepository()
-)
+class ProfileService:
+    def __init__(self, repo: UserRepository):
+        self.repo = repo
+
+    async def update_profile(
+        self,
+        user: User,
+        data: ProfileUpdateData,
+        avatar: UploadFile | None
+    ):
+        if avatar:
+            file_name = await save_avatar_to_disk(avatar)
+
+            if user.avatar:
+                user.avatar.file_path = file_name
+            else:
+                user.avatar = UserAvatar(file_path=file_name)
+
+        await self.repo.update_user(user, data)
+
